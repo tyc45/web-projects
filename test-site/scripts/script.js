d6851684dae44dc385b5265d29bb45a8c746dff3
tyc45 <tyc450@gmail.com>
2021-10-07 13:38:42 +0200

    Did some JS practice

--- test-site/scripts/script.js
+++ test-site/scripts/script.js
@@ -1,2 +1,19 @@
-let userName = prompt("What's your name?");
-alert(`You are ${userName}, welcome!`);
+let login = prompt("Who're you?");
+if (login == "Admin"){
+  let mdp = prompt("Password please?");
+  if (mdp == "TheMaster"){
+    alert("Welcome!");
+  }
+  else if (mdp == "" || mdp == null){
+    alert("Cancelled!");
+  }
+  else{
+    alert("Wrond Password!")
+  }
+}
+else if (login == "" || null){
+  alert("Cancelled!");
+}
+else{
+  alert("I don't know you!");
+}
