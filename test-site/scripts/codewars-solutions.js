7ae30bf8cc5d6f2c2c09f571027177c5b5f031b2
tyc45 <tyc450@gmail.com>
2021-10-12 16:10:42 +0200

    Freuded!

--- test-site/scripts/codewars-solutions.js
+++ test-site/scripts/codewars-solutions.js
@@ -4,3 +4,37 @@ function even_or_odd(number) {
   if(number % 2 == 0) return "Even";
   return "Odd";
 }
+
+
+// Traffic light
+
+function updateLight(current) {
+  if(current === "green"){
+    return "yellow";
+  }
+  else if(current === "yellow"){
+    return "red";
+  }
+  return "green";
+}
+
+
+// Convering an Hex string to decimals
+
+function hexToDec(hexString){
+  return parseInt(hexString, 16);
+}
+
+
+// Converting every word in a string to "sex" (because Freud)
+
+function toFreud(string) {
+  let result = "";
+  if(string && string.length > 0){
+  result += "sex";
+  for(i = 1; i < string.split(" ").length; i++){
+    result += " sex";
+  }
+  }
+return result;
+}
